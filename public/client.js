82d4b2cf284fca62ed16cd57a1d316e59f6802b5
Glitch (airtable-example) <none>
2020-04-23 03:24:07 +0000

    🙆🌒 Checkpoint ./public/style.css:671814/173 ./views/index.html:671814/37 ./public/client.js:671814/409

--- public/client.js
+++ public/client.js
@@ -1,6 +1,13 @@
 // client-side js
 // run by the browser each time your view template is loaded
 
-$(function() {
+(async function(d) {
   
-});
+  const req = await fetch('/pies')
+  const resp = await req.json()
+  
+  console.log(resp)
+  
+  
+  
+})(document);
