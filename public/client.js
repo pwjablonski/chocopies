8f07fb2eb983f79415b9ed0d08c0daf57a09cc79
Glitch (airtable-example) <none>
2020-04-27 15:39:11 +0000

    🎇⚓️ Checkpoint ./public/client.js:671814/2524

--- public/client.js
+++ public/client.js
@@ -13,13 +13,6 @@
   }
 
   function displayPies(pies) {
-    let width = "100%"
-    let height = "100%"
-    let svg = d3
-        .select(".map")
-        .append("svg")
-        .style("width", width)
-        .style("height", height);
 
     // const map = document.querySelector(".map-svg");
     // pies.forEach(pie => {
@@ -32,4 +25,39 @@
     //   map.appendChild(pieRect)
     // })
   }
+  
+  
+  async function drawMap(){
+    let width = "100%"
+    let height = "100%"
+    
+    const data = d3.json("https://gist.githubusercontent.com/milafrerichs/78ef5702db2dc514fc2bed465d58406b/raw/f1366ee2a83a9afb1dd2427e9cbd4cd3db8d87ca/bundeslaender_simplify200.geojson")
+    
+    var albersProjection = d3.geoAlbers()
+    .scale( 190000 )
+    .rotate( [71.057,0] )
+    .center( [0, 42.313] )
+    .translate( [width/2,height/2] );
+    
+    var geoPath = d3.geoPath()
+    .projection( albersProjection );
+    
+    
+    let svg = d3
+        .select(".map")
+        .append("svg")
+        .style("width", width)
+        .style("height", height);
+    
+    let g = svg.append( "g" );
+    
+    g.selectAll( "path" )
+      .data( neighborhoods_json.features )
+      .enter()
+      .append( "path" )
+      .attr( "fill", "#ccc" )
+      .attr( "stroke", "#333")
+      .attr( "d", geoPath ); 
+  }
+  
 })(document, d3);
