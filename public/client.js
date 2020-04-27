c7a3aeb075c4a809939a803989dc904addb45798
Glitch (airtable-example) <none>
2020-04-27 15:27:13 +0000

    🌽👗 Checkpoint ./public/client.js:671814/2203 ./views/index.html:671814/55

--- public/client.js
+++ public/client.js
@@ -13,28 +13,13 @@
   }
 
   function displayPies(pies) {
-    d3.json("data.geojson").then(function(bb) {
-      let width = 200,
-        height = 200;
-      let projection = d3.geoEqualEarth();
-      projection.fitSize([width, height], bb);
-      let geoGenerator = d3.geoPath().projection(projection);
-
-      let svg = d3
-        .select("body")
+    let width = "100%"
+    let height = "100%"
+    let svg = d3
+        .select(".map")
         .append("svg")
         .style("width", width)
         .style("height", height);
-
-      svg
-        .append("g")
-        .selectAll("path")
-        .data(bb.features)
-        .join("path")
-        .attr("d", geoGenerator)
-        .attr("fill", "#088")
-        .attr("stroke", "#000");
-    });
 
     // const map = document.querySelector(".map-svg");
     // pies.forEach(pie => {
