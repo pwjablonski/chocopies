903a46ced15bd91764ccf35f325b900a2eb72fe8
Glitch (airtable-example) <none>
2020-04-30 17:21:06 +0000

    🧀🍳 Checkpoint ./public/index.js:671814/6639 ./public/browse.js:671814/14410

--- public/browse.js
+++ public/browse.js
@@ -1,98 +1,114 @@
 // client-side js
 // run by the browser each time your view template is loaded
 
-(async function(d,) {
-
-  
+(async function(d) {
   const pies = await fetchPies();
   const pixels = await fetchPixels();
-  
-  drawMap(pixels, pies)
-  
-  
+
+  drawMap(pixels, pies);
+
   async function fetchPies() {
     const req = await fetch("/pies");
     const resp = await req.json();
     return resp;
   }
-  
+
   async function fetchPixels() {
     const req = await fetch("korea.json");
     const resp = await req.json();
     return resp;
   }
-  
-  document.addEventListener('click',function(e){
-    console.log(e.target)
-    if(e.target.classList.contains("pie")){
-      window.location.href = `/chocopie/${e.target.id}/send`
-    }
-  })
-  
-  function drawMap(pixels, pies) {
-    const map = document.querySelector(".map-svg")
-    
-    const mapgroup = document.createElementNS("http://www.w3.org/2000/svg", 'g');
-    pixels.data.forEach((pixel, i) => {
-      const y = Math.floor(i / pixels.width)
-      const x = i % pixels.width
-      const pixelRect = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
-      const pixelgroup = document.createElementNS("http://www.w3.org/2000/svg", 'g');
-      pixelRect.classList.add("pie")
-      pixelRect.setAttribute("height", "0.98");
-      pixelRect.setAttribute("width", "0.98")
-      pixelRect.setAttribute("x", 1 * x)
-      pixelRect.setAttribute("y", 1 * y)
-      if(pixel){
-        // pixelRect.style.fill = "red"
-        // pixelRect.style.stroke = "red"
-        pixelRect.id = i
-      } else{
-        // pixelRect.style.fill = "white"
-        pixelRect.style.stroke = "white"
-      }
-      pixelgroup.appendChild(pixelRect)
-      mapgroup.appendChild(pixelgroup)
-    })
-    
-    mapgroup.setAttribute("transform", "scale(1)")
-    map.appendChild(mapgroup)
-    
-    pies.forEach((pie)=> {
-      const pixelRect = document.getElementById(`${pie.fields.location}`);
-      const y = Math.floor(pie.fields.location / pixels.width)
-      const x = pie.fields.location % pixels.width
-      if(pixelRect){
-        // pixelRect.style.fill = "blue"
-        const text = document.createElementNS("http://www.w3.org/2000/svg", 'text');
-        const title = document.createElementNS("http://www.w3.org/2000/svg", 'tspan');
-        const name = document.createElementNS("http://www.w3.org/2000/svg", 'tspan');
-        const date = document.createElementNS("http://www.w3.org/2000/svg", 'tspan');
-        title.textContent = "SHARED BY"
-        name.textContent = "PETER"
-        date.textContent ="4-20-20"
-        
-        title.setAttribute("dy", "0em")
-        title.setAttribute("x", (1 * x) + 0.5)
-        name.setAttribute("dy", "2em")
-        name.setAttribute("x", (1 * x) + 0.5)
-        date.setAttribute("dy", "2em")
-        date.setAttribute("x", (1 * x) + 0.5)
 
-        text.style.fill = "white"
-        text.setAttribute("x", (1 * x))
-        text.setAttribute("y", (1 * y) + 0.3)
-        text.setAttribute("text-anchor", "middle")
-        text.setAttribute("font-size", "0.005em")
-        
-        text.appendChild(title)
-        text.appendChild(name)
-        text.appendChild(date)
-        
-        pixelRect.style.fill = "#0080ff"
-        pixelRect.parentElement.appendChild(text)
+  document.addEventListener("click", function(e) {
+    console.log(e.target);
+    if (e.target.classList.contains("pie")) {
+      window.location.href = `/chocopie/${e.target.id}/send`;
+    }
+  });
+
+  function drawMap(pixels, pies) {
+    const map = document.querySelector(".map-svg");
+
+    const mapgroup = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "g"
+    );
+    pixels.data.forEach((pixel, i) => {
+      if (pixel) {
+        const y = Math.floor(i / pixels.width);
+        const x = i % pixels.width;
+        const pixelRect = document.createElementNS(
+          "http://www.w3.org/2000/svg",
+          "rect"
+        );
+        const pixelgroup = document.createElementNS(
+          "http://www.w3.org/2000/svg",
+          "g"
+        );
+        pixelRect.classList.add("pie");
+        pixelRect.setAttribute("height", "0.98");
+        pixelRect.setAttribute("width", "0.98");
+        pixelRect.setAttribute("x", 1 * x);
+        pixelRect.setAttribute("y", 1 * y);
+        pixelRect.id = i;
+        pixelgroup.appendChild(pixelRect);
+        mapgroup.appendChild(pixelgroup);
       }
-    })
+      // else {
+      //   // pixelRect.style.fill = "white"
+      //   pixelRect.style.stroke = "white";
+      // }
+    });
+
+    mapgroup.setAttribute("transform", "scale(1)");
+    map.appendChild(mapgroup);
+
+    pies.forEach(pie => {
+      const pixelRect = document.getElementById(`${pie.fields.location}`);
+      const y = Math.floor(pie.fields.location / pixels.width);
+      const x = pie.fields.location % pixels.width;
+      if (pixelRect) {
+        // pixelRect.style.fill = "blue"
+        const text = document.createElementNS(
+          "http://www.w3.org/2000/svg",
+          "text"
+        );
+        const title = document.createElementNS(
+          "http://www.w3.org/2000/svg",
+          "tspan"
+        );
+        const name = document.createElementNS(
+          "http://www.w3.org/2000/svg",
+          "tspan"
+        );
+        const date = document.createElementNS(
+          "http://www.w3.org/2000/svg",
+          "tspan"
+        );
+        title.textContent = "SHARED BY";
+        name.textContent = "PETER";
+        date.textContent = "4-20-20";
+
+        title.setAttribute("dy", "0em");
+        title.setAttribute("x", 1 * x + 0.5);
+        name.setAttribute("dy", "2em");
+        name.setAttribute("x", 1 * x + 0.5);
+        date.setAttribute("dy", "2em");
+        date.setAttribute("x", 1 * x + 0.5);
+
+        text.style.fill = "white";
+        text.setAttribute("x", 1 * x);
+        text.setAttribute("y", 1 * y + 0.3);
+        text.setAttribute("text-anchor", "middle");
+        text.setAttribute("font-size", "0.005em");
+
+        text.appendChild(title);
+        text.appendChild(name);
+        text.appendChild(date);
+
+        pixelRect.style.fill = "#0080ff";
+        pixelRect.parentElement.appendChild(text);
+      }
+    });
   }
-  
 })(document);
--- public/index.js
+++ public/index.js
@@ -1,61 +1,56 @@
 // client-side js
 // run by the browser each time your view template is loaded
 
-(async function(d,) {
-
-  
+(async function(d) {
   const pies = await fetchPies();
   const pixels = await fetchPixels();
-  
-  drawMap(pixels, pies)
-  
-  
+
+  drawMap(pixels, pies);
+
   async function fetchPies() {
     const req = await fetch("/pies");
     const resp = await req.json();
     return resp;
   }
-  
+
   async function fetchPixels() {
     const req = await fetch("korea.json");
     const resp = await req.json();
     return resp;
   }
-  
+
   function drawMap(pixels, pies) {
-    const map = document.querySelector(".map-svg")
-    
-    const group = document.createElementNS("http://www.w3.org/2000/svg", 'g');
+    const map = document.querySelector(".map-svg");
+
+    const group = document.createElementNS("http://www.w3.org/2000/svg", "g");
     pixels.data.forEach((pixel, i) => {
-      const y = Math.floor(i / pixels.width)
-      const x = i % pixels.width
-      const pixelRect = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
-      pixelRect.classList.add("pie")
-      pixelRect.setAttribute("height", "1");
-      pixelRect.setAttribute("width", "1")
-      pixelRect.setAttribute("x", 1 * x)
-      pixelRect.setAttribute("y", 1 * y)
-      if(pixel){
-        // pixelRect.style.fill = "red"
-        pixelRect.style.stroke = "red"
-        pixelRect.id = i
-      } else{
-        pixelRect.style.fill = "white"
-        pixelRect.style.stroke = "white"
+      if (pixel) {
+        const y = Math.floor(i / pixels.width);
+        const x = i % pixels.width;
+        const pixelRect = document.createElementNS(
+          "http://www.w3.org/2000/svg",
+          "rect"
+        );
+        pixelRect.classList.add("pie");
+        pixelRect.setAttribute("height", "1");
+        pixelRect.setAttribute("width", "1");
+        pixelRect.setAttribute("x", 1 * x);
+        pixelRect.setAttribute("y", 1 * y);
+        pixelRect.style.stroke = "red";
+        pixelRect.id = i;
+        group.appendChild(pixelRect);
       }
-      group.appendChild(pixelRect)
-    })
-    
-    group.setAttribute("transform", "scale(1)")
-    map.appendChild(group)
-    
-    pies.forEach((pie)=> {
+    });
+
+    group.setAttribute("transform", "scale(1)");
+    map.appendChild(group);
+
+    pies.forEach(pie => {
       const pixelRect = document.getElementById(`${pie.fields.location}`);
-      if(pixelRect){
-        pixelRect.style.fill = "blue"
-        pixelRect.style.stroke = "blue"
+      if (pixelRect) {
+        pixelRect.style.fill = "blue";
+        pixelRect.style.stroke = "blue";
       }
-    })
+    });
   }
-  
 })(document);
