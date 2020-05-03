2af0c925a4f43d32e004b5dfa69cd9d0d774a0ae
Glitch (airtable-example) <none>
2020-05-03 03:03:16 +0000

    💳💗 Checkpoint ./views/checkout.html:671814/182 ./public/checkout.js:671814/4937

---
+++ public/checkout.js
@@ -0,0 +1,15 @@
+(async function(d) {
+  console.log(window.location.pathname.split('/')[2])
+  
+//   async function fetchPies() {
+//     const req = await fetch("/pies");
+//     const resp = await req.json();
+//     return resp;
+//   }
+
+//   document.addEventListener("click", function(e) {
+//     if (e.target.classList.contains("pie")) {
+//       window.location.href = `/chocopie/${e.target.id}/send`;
+//     }
+//   });
+})(document);
