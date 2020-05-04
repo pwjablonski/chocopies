1eb00a8c75f24e11f61ae1956feb426245f92547
Glitch (airtable-example) <none>
2020-05-04 23:44:33 +0000

    🍳🎣 Checkpoint ./views/register.html:671814/7 ./public/register.html:671814/529 ./public/register.js:671814/504

---
+++ public/register.js
@@ -0,0 +1,20 @@
+(async function(d) {
+//   const pieId = window.location.pathname.split("/")[2];
+
+//   async function sendPie(pieId) {
+//     const req = await fetch("/pies", {
+//       method: "post",
+//       body: JSON.stringify({ pieId }),
+//       headers: { "Content-Type": "application/json" }
+//     });
+//     const resp = await req.json();
+//     return resp;
+//   }
+  
+
+
+  document.addEventListener("submit", function(e) {
+    
+    e.preventDefault();
+  });
+})(document);
