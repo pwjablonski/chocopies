5edda995f0ca49094854cd149bf01c2d63677567
Glitch (airtable-example) <none>
2020-05-05 17:35:18 +0000

    📙🍙 Checkpoint ./views/index.html:671814/721 ./public/style.css:671814/755 ./public/index.js:671814/5120

--- public/index.js
+++ public/index.js
@@ -2,8 +2,8 @@
 // run by the browser each time your view template is loaded
 
 (async function(d) {
-  const {total, claimed, pies} = await fetchPies();
-  
+  const { total, claimed, pies } = await fetchPies();
+
   drawData(total, claimed);
   drawMap(pies);
 
@@ -12,42 +12,56 @@
     const resp = await req.json();
     return resp;
   }
-  
-  function drawData(total, claimed){
+
+  function drawData(total, claimed) {
     const totalDiv = document.querySelector("#totalPies");
     const claimedDiv = document.querySelector("#piesClaimed");
-    const moneyDiv = document.querySelector("#moneyRaised");  
+    const moneyDiv = document.querySelector("#moneyRaised");
     totalDiv.innerHTML = total;
-    claimedDiv.innerHTML= claimed;
+    claimedDiv.innerHTML = claimed;
     moneyDiv.innerHTML = `$ ${claimed}`;
   }
-  
 
   function drawMap(pies) {
     const map = document.querySelector(".map-svg");
 
     const group = document.createElementNS("http://www.w3.org/2000/svg", "g");
-    pies.forEach((pie) => {
-        const pieRect = document.createElementNS(
-          "http://www.w3.org/2000/svg",
-          "rect"
-        );
-        pieRect.classList.add("pie");
-        pieRect.setAttribute("height", "1");
-        pieRect.setAttribute("width", "1");
-        pieRect.setAttribute("x", pie.x);
-        pieRect.setAttribute("y", pie.y);
-        pieRect.id = pie.id;
-        if (pie.isClaimed) {
-          pieRect.style.fill = "blue";
-          pieRect.style.stroke = "blue";
-        } else {
-          pieRect.style.stroke = "red";
-        }
-        group.appendChild(pieRect);
+    pies.forEach(pie => {
+      const pieRect = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "rect"
+      );
+      pieRect.classList.add("pie");
+      pieRect.setAttribute("height", "1");
+      pieRect.setAttribute("width", "1");
+      pieRect.setAttribute("x", pie.x);
+      pieRect.setAttribute("y", pie.y);
+      pieRect.id = pie.id;
+      if (pie.isClaimed) {
+        pieRect.style.fill = "blue";
+        pieRect.style.stroke = "blue";
+      } else {
+        pieRect.style.stroke = "red";
+      }
+      group.appendChild(pieRect);
     });
 
     group.setAttribute("transform", "scale(1)");
     map.appendChild(group);
   }
+
+  let modalBtn = document.getElementById("modal-btn");
+  let modal = document.querySelector(".modal");
+  let closeBtn = document.querySelector(".close-btn");
+  d.addEventListener("click", function(e) {
+    if (e.target == modal) {
+      modal.style.display = "none";
+    }
+    if(e.target == modalBtn){
+      modal.style.display = "block";
+    }
+    if(e.target == closeBtn){
+      modal.style.display = "none";
+    }
+  });
 })(document);
