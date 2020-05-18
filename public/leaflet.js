d9510df229d7f59b0ca74a4d0aea9c05fba991c2
Glitch (chocopie) <none>
2020-05-18 01:33:35 +0000

    🐩🎥 Checkpoint ./public/leaflet.js:671814/15037 ./public/style.css:671814/21

--- public/leaflet.js
+++ public/leaflet.js
@@ -11,19 +11,32 @@
   drawData(total, claimed);
   // drawMap(pies, "map-svg");
   // drawMap(pies, "zoom-map-svg");
-  
-  
-  
-  var mymap = L.map('mapid').setView([51.505, -0.09], 5);
-  
-  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoicHdqYWJsb25za2kiLCJhIjoiY2s5dW5wdnh0MDBzYTNtbHFtZWRtbmw3YSJ9.eHxCyVJuJmXzblybi0S9_w'
-}).addTo(mymap);
+
+  var mymap = L.map("mapid").setView([38, 127], 5);
+
+  L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: "mapbox/streets-v11",
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken:
+        "pk.eyJ1IjoicHdqYWJsb25za2kiLCJhIjoiY2s5dW5wdnh0MDBzYTNtbHFtZWRtbmw3YSJ9.eHxCyVJuJmXzblybi0S9_w"
+    }
+  ).addTo(mymap);
+  var svgElement = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "svg"
+  );
+  svgElement.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+  svgElement.setAttribute("viewBox", "0 0 200 200");
+  svgElement.innerHTML =
+    '<rect width="200" height="200"/><rect x="75" y="23" width="50" height="50" style="fill:red"/><rect x="75" y="123" width="50" height="50" style="fill:#0013ff"/>';
+  var svgElementBounds = [[46, 123], [27, 130]];
+  L.svgOverlay(svgElement, svgElementBounds).addTo(mymap);
 
   async function fetchPies() {
     const req = await fetch("/pies");
@@ -40,123 +53,123 @@
     moneyDiv.innerHTML = `$ ${claimed}`;
   }
 
-//   function drawMap(pies, selector) {
-//     const map = document.querySelector(`.${selector}`);
+  //   function drawMap(pies, selector) {
+  //     const map = document.querySelector(`.${selector}`);
 
-//     const mapgroup = document.createElementNS(
-//       "http://www.w3.org/2000/svg",
-//       "g"
-//     );
+  //     const mapgroup = document.createElementNS(
+  //       "http://www.w3.org/2000/svg",
+  //       "g"
+  //     );
 
-//     pies.forEach(pie => {
-//       const pieRect = document.createElementNS(
-//         "http://www.w3.org/2000/svg",
-//         "rect"
-//       );
+  //     pies.forEach(pie => {
+  //       const pieRect = document.createElementNS(
+  //         "http://www.w3.org/2000/svg",
+  //         "rect"
+  //       );
 
-//       const pieGroup = document.createElementNS(
-//         "http://www.w3.org/2000/svg",
-//         "g"
-//       );
+  //       const pieGroup = document.createElementNS(
+  //         "http://www.w3.org/2000/svg",
+  //         "g"
+  //       );
 
-//       pieRect.classList.add("pie");
-//       pieRect.classList.add(`${selector}-pie`);
-//       pieRect.setAttribute("height", "0.98");
-//       pieRect.setAttribute("width", "0.98");
-//       pieRect.setAttribute("x", 1 * pie.x);
-//       pieRect.setAttribute("y", 1 * pie.y);
-//       pieRect.id = pie.id;
-//       pieGroup.appendChild(pieRect);
-//       mapgroup.appendChild(pieGroup);
+  //       pieRect.classList.add("pie");
+  //       pieRect.classList.add(`${selector}-pie`);
+  //       pieRect.setAttribute("height", "0.98");
+  //       pieRect.setAttribute("width", "0.98");
+  //       pieRect.setAttribute("x", 1 * pie.x);
+  //       pieRect.setAttribute("y", 1 * pie.y);
+  //       pieRect.id = pie.id;
+  //       pieGroup.appendChild(pieRect);
+  //       mapgroup.appendChild(pieGroup);
 
-//       if (pie.isClaimed) {
-//         drawClaimedPie(pieRect);
-//       } else {
-//         // pieRect.dataset.toggle = "modal";
-//         // pieRect.dataset.target = "#sendPie";
-//       }
-//     });
+  //       if (pie.isClaimed) {
+  //         drawClaimedPie(pieRect);
+  //       } else {
+  //         // pieRect.dataset.toggle = "modal";
+  //         // pieRect.dataset.target = "#sendPie";
+  //       }
+  //     });
 
-//     map.appendChild(mapgroup);
-//   }
+  //     map.appendChild(mapgroup);
+  //   }
 
-//   function drawClaimedPie(pieRect) {
-//     const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
-//     const title = document.createElementNS(
-//       "http://www.w3.org/2000/svg",
-//       "tspan"
-//     );
-//     const name = document.createElementNS(
-//       "http://www.w3.org/2000/svg",
-//       "tspan"
-//     );
-//     const date = document.createElementNS(
-//       "http://www.w3.org/2000/svg",
-//       "tspan"
-//     );
-//     title.textContent = "SHARED BY";
-//     name.textContent = "PETER";
-//     date.textContent = "4-20-20";
+  //   function drawClaimedPie(pieRect) {
+  //     const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+  //     const title = document.createElementNS(
+  //       "http://www.w3.org/2000/svg",
+  //       "tspan"
+  //     );
+  //     const name = document.createElementNS(
+  //       "http://www.w3.org/2000/svg",
+  //       "tspan"
+  //     );
+  //     const date = document.createElementNS(
+  //       "http://www.w3.org/2000/svg",
+  //       "tspan"
+  //     );
+  //     title.textContent = "SHARED BY";
+  //     name.textContent = "PETER";
+  //     date.textContent = "4-20-20";
 
-//     title.setAttribute("dy", "0em");
-//     title.setAttribute("x", 1 * pieRect.getAttribute("x") + 0.5);
-//     name.setAttribute("dy", "2em");
-//     name.setAttribute("x", 1 * pieRect.getAttribute("x") + 0.5);
-//     date.setAttribute("dy", "2em");
-//     date.setAttribute("x", 1 * pieRect.getAttribute("x") + 0.5);
+  //     title.setAttribute("dy", "0em");
+  //     title.setAttribute("x", 1 * pieRect.getAttribute("x") + 0.5);
+  //     name.setAttribute("dy", "2em");
+  //     name.setAttribute("x", 1 * pieRect.getAttribute("x") + 0.5);
+  //     date.setAttribute("dy", "2em");
+  //     date.setAttribute("x", 1 * pieRect.getAttribute("x") + 0.5);
 
-//     text.style.fill = "white";
-//     text.setAttribute("x", 1 * pieRect.getAttribute("y"));
-//     text.setAttribute("y", 1 * pieRect.getAttribute("y") + 0.3);
-//     text.setAttribute("text-anchor", "middle");
-//     text.setAttribute("font-size", "0.005em");
+  //     text.style.fill = "white";
+  //     text.setAttribute("x", 1 * pieRect.getAttribute("y"));
+  //     text.setAttribute("y", 1 * pieRect.getAttribute("y") + 0.3);
+  //     text.setAttribute("text-anchor", "middle");
+  //     text.setAttribute("font-size", "0.005em");
 
-//     text.appendChild(title);
-//     text.appendChild(name);
-//     text.appendChild(date);
+  //     text.appendChild(title);
+  //     text.appendChild(name);
+  //     text.appendChild(date);
 
-//     pieRect.style.filter = "none";
-//     pieRect.style.fill = "#0080ff";
-//     pieRect.parentElement.appendChild(text);
-//   }
+  //     pieRect.style.filter = "none";
+  //     pieRect.style.fill = "#0080ff";
+  //     pieRect.parentElement.appendChild(text);
+  //   }
 
-//   d.addEventListener("click", function(e) {
-//     if (e.target.dataset.toggle == "modal") {
-//       let modal = document.querySelector(e.target.dataset.target);
-//       modal.style.display = "block";
-//     }
-//     if (e.target.dataset.dismiss == "modal") {
-//       let modals = document.querySelectorAll(".modal");
-//       modals.forEach(function(modal) {
-//         modal.style.display = "none";
-//       });
-//     }
-//     if (e.target.classList.contains("modal")) {
-//       e.target.style.display = "none";
-//     }
+  //   d.addEventListener("click", function(e) {
+  //     if (e.target.dataset.toggle == "modal") {
+  //       let modal = document.querySelector(e.target.dataset.target);
+  //       modal.style.display = "block";
+  //     }
+  //     if (e.target.dataset.dismiss == "modal") {
+  //       let modals = document.querySelectorAll(".modal");
+  //       modals.forEach(function(modal) {
+  //         modal.style.display = "none";
+  //       });
+  //     }
+  //     if (e.target.classList.contains("modal")) {
+  //       e.target.style.display = "none";
+  //     }
 
-//     // if (e.target.classList.contains("zoom-map-svg-pie")) {
-//     //   let modal = document.querySelector("#sendPie");
-//     //   modal.style.display = "block";
-//     //   selectedPieId = e.target.id;
-//     // }
+  //     // if (e.target.classList.contains("zoom-map-svg-pie")) {
+  //     //   let modal = document.querySelector("#sendPie");
+  //     //   modal.style.display = "block";
+  //     //   selectedPieId = e.target.id;
+  //     // }
 
-//     if (e.target.classList.contains("map-svg-pie")) {
-//       let modal = document.querySelector("#viewPies");
-//       modal.style.display = "block";
-//       console.log(e.target.getAttribute("x"), e.target.getAttribute("y"));
-//       const panZoomInstance = sPZ(".zoom-map-svg", {
-//         maxZoom: 30,
-//         zoomEnabled: false,
-//       });
+  //     if (e.target.classList.contains("map-svg-pie")) {
+  //       let modal = document.querySelector("#viewPies");
+  //       modal.style.display = "block";
+  //       console.log(e.target.getAttribute("x"), e.target.getAttribute("y"));
+  //       const panZoomInstance = sPZ(".zoom-map-svg", {
+  //         maxZoom: 30,
+  //         zoomEnabled: false,
+  //       });
 
-//       panZoomInstance.zoom(20);
-//       // const sizes = panZoomInstance.getSizes()
-//       // const zoomX = (sizes.width / sizes.viewBox.width) * e.target.getAttribute('x')
-//       // const zoomY = (sizes.height / sizes.viewBox.height) * e.target.getAttribute('y')
-//       // panZoomInstance.zoomAtPoint(20, {x: zoomX, y: zoomY});
-//     }
-//   });
+  //       panZoomInstance.zoom(20);
+  //       // const sizes = panZoomInstance.getSizes()
+  //       // const zoomX = (sizes.width / sizes.viewBox.width) * e.target.getAttribute('x')
+  //       // const zoomY = (sizes.height / sizes.viewBox.height) * e.target.getAttribute('y')
+  //       // panZoomInstance.zoomAtPoint(20, {x: zoomX, y: zoomY});
+  //     }
+  //   });
 
   d.addEventListener("dblclick", function(e) {
     if (e.target.classList.contains("zoom-map-svg-pie")) {
