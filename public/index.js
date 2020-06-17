2969bdbe6be3e5db664b59138f79cc5148ffe634
Glitch (eatchocopietogether) <none>
2020-06-17 21:29:02 +0000

    🐄🎑 Checkpoint ./public/index.js:17514353/372

--- public/index.js
+++ public/index.js
@@ -193,12 +193,6 @@
 
   function drawMainMap(pies) {
     const loading = d3.select(".main-map__loading").style("display", "none");
-    
-    const loading = d3
-    .select("#div1")
-    .select("#div2")
-    .select("#div3")
-    .attr("class","introAnimate");
 
     const svg = d3
       .select("#main-map")
