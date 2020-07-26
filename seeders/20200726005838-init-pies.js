f60f4a3b144d21b2c7b277ca25794abed567f282
Glitch (eatchocopietogether) <none>
2020-07-26 01:16:26 +0000

    ⏲🛀 Checkpoint ./seeders/20200726005838-init-pies.js:671814/6616

--- seeders/20200726005838-init-pies.js
+++ seeders/20200726005838-init-pies.js
@@ -1,6 +1,5 @@
-'use strict';
+"use strict";
 const Jimp = require("jimp");
-
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
@@ -12,31 +11,37 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-    */
-    
-  const image = await Jimp.read(
-    "https://cdn.glitch.com/1fa742a9-ec9d-49fb-8d8b-1aaa0efe3e2c%2Fkorea-2500.png?v=1593401598746"
-  );
+     */
 
-  const width = image.bitmap.width;
-  const height = image.bitmap.height;
+    const image = await Jimp.read(
+      "https://cdn.glitch.com/1fa742a9-ec9d-49fb-8d8b-1aaa0efe3e2c%2Fkorea-2500.png?v=1593401598746"
+    );
+
+    const width = image.bitmap.width;
+    const height = image.bitmap.height;
+    const pies = []
 
     for (var y = 0; y < height; y++) {
       for (var x = 0; x < width; x++) {
         var pixel = Jimp.intToRGBA(image.getPixelColor(x, y));
         if (!(pixel.r === 255 && pixel.g === 255 && pixel.b === 255)) {
-          queryInterface.create('Pies', {
-            x,
-            y,
-            lat: 43 - y * 0.05,
-            lng: 124 + x * 0.1
-          });
+          pies.push(
+            {
+              x,
+              y,
+              lat: 43 - y * 0.05,
+              lng: 124 + x * 0.1
+            }
+          );
         }
       }
     }
+    
+    queryInterface.bulkInsert("Pies", pies);
+    
   },
 
   down: async (queryInterface, Sequelize) => {
-     await queryInterface.bulkDelete('Pies', null, {});
+    await queryInterface.bulkDelete("Pies", null, {});
   }
 };
