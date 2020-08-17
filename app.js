09cb20c61e9bc9446e39c663abc6f5a83949cf3c
Glitch (eatchocopietogether) <none>
2020-08-17 18:17:46 +0000

    👴😬 Checkpoint ./app.js:671814/134

--- app.js
+++ app.js
@@ -11,17 +11,18 @@ const { body } = require("express-validator");
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
-// passport.use(new GoogleStrategy({
-//     clientID: process.env.GOOGLE_CLIENT_ID,
-//     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//     callbackURL: "https://eatchocopietogether.com/auth/google/callback"
-//   },
-//   function(accessToken, refreshToken, profile, done) {
-//        db.User.findOrCreate({ googleId: profile.id }, function (err, user) {
-//          return done(err, user);
-//        });
-//   }
-// ));
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "https://eatchocopietogether.com/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+      console.log(profile)
+       // db.User.findOrCreate({ googleId: profile.id }, function (err, user) {
+       //   return done(err, user);
+       // });
+  }
+));
 
 const app = express();
 
@@ -53,14 +54,14 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 // ROUTES
 
 
-// app.get('/auth/google',
-//   passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
 
-// app.get('/auth/google/callback', 
-//   passport.authenticate('google', { failureRedirect: '/login' }),
-//   function(req, res) {
-//     res.redirect('/');
-//   });
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
 
 app.get("/", function(request, response) {
   response.render("pages/index");
