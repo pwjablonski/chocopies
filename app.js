38f11c3f19bd17249ea503c38d1f2cd0c7cede3a
Glitch (eatchocopietogether) <none>
2020-08-18 15:35:09 +0000

    🚄🌑 Checkpoint ./app.js:671814/444

--- app.js
+++ app.js
@@ -25,6 +25,25 @@ passport.use(new GoogleStrategy({
   }
 ));
 
+// Used to stuff a piece of information into a cookie
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+// Used to decode the received cookie and persist session
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
+// Middleware to check if the user is authenticated
+function isUserAuthenticated(req, res, next) {
+    if (req.user) {
+        next();
+    } else {
+        res.send('You must login!');
+    }
+}
+
 const app = express();
 
 app.set("views", __dirname + "/views");
