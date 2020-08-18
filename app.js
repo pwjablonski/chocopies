b24920dc7c0dd9ff1f4b0f2b73da953262c633d3
Glitch (eatchocopietogether) <none>
2020-08-18 15:46:46 +0000

    🐙🍿 Checkpoint ./app.js:671814/44

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ passport.use(new GoogleStrategy({
   },
   function(accessToken, refreshToken, profile, done) {
       console.log(profile)
+       // done(null, profile);
        // db.User.findOrCreate({ googleId: profile.id }, function (err, user) {
        //   return done(err, user);
        // });
@@ -118,9 +119,9 @@ app.get("/shop", function(request, response) {
   response.render("pages/shop");
 });
 
-// app.get("/manage", function(request, response) {
-//   response.render("pages/manage");
-// });
+app.get("/manage", function(request, response) {
+  response.render("pages/manage");
+});
 
 app.get("/privacy", async function(request, response) {
   // db.Pie.update({eatenAt:null, sentAt:null, senderName: null, senderEmail:null, recipientName: null, recipientEmail: null, subscribedSender: null, message:null}, {where:{id:1}})
